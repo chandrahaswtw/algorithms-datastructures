c74cc4c82bafd7dbab16b0b375d09e9f8c11da15
chandrahaswtw <chandrahaswtw@gmail.com>
2026-01-13 17:29:53 +0530

    Find substrings

---
+++ dsa-practice/2_findSubstrings.ts
@@ -0,0 +1,54 @@
+/*
+Problem statement: Find all possible substrings from the given string. For example if abc is given, it should return:
+abc, ab, ac, bc, a, b, c, ''
+
+Hint:
+
+Consider the string abc.
+
+We break the problem recursively:
+
+abc
+a + bc
+    b + c
+        c + ""
+            ""
+
+When we reach the empty string (""), we return:
+
+[""]
+
+Now, while returning back up the call stack:
+
+At character 'c':
+Old result: [""]
+New result by prepending 'c': ["c"]
+Combined result (preserving old + new):
+["", "c"]
+
+At character 'b':
+Old result: ["", "c"]
+New result by prepending 'b': ["b", "bc"]
+Combined result:
+["", "c", "b", "bc"]
+
+At character 'a':
+Old result: ["", "c", "b", "bc"]
+New result by prepending 'a': ["a", "ac", "ab", "abc"]
+Final combined result:
+["", "c", "b", "bc", "a", "ac", "ab", "abc"]
+
+*/
+
+const findSubstrings = (str: string): Array<string> => {
+  if (!str.length) {
+    return [""];
+  }
+
+  const firstChar = str.charAt(0);
+  const restSubstrings = findSubstrings(str.substring(1));
+  const withChar = restSubstrings.map((s) => firstChar + s);
+  return [...restSubstrings, ...withChar];
+};
+
+console.log(findSubstrings("abc"));
