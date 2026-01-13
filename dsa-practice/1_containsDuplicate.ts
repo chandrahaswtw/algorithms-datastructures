762f1b6b7ff0c938fc2805094d33499482b4dd1a
chandrahaswtw <chandrahaswtw@gmail.com>
2026-01-13 17:29:35 +0530

    Check if duplicate exists

---
+++ dsa-practice/1_containsDuplicate.ts
@@ -0,0 +1,22 @@
+/*
+Problem statement: Given an array, return true if any value appears atleast twice in the array. Return false if all are distinct.
+
+Hint:
+- We declare an object (Hashmap), and we iterate over the array.
+- The moment it finds the duplicate key it returns true else false.
+*/
+
+const containsDuplicate = (nums: Array<number>) => {
+  const numHash: Record<number, boolean> = {};
+  for (let i of nums) {
+    if (numHash[i]) {
+      return true;
+    } else {
+      numHash[i] = true;
+    }
+  }
+  return false;
+};
+
+console.log(containsDuplicate([1, 2, 3, 4, 5, 6, 7]));
+console.log(containsDuplicate([1, 2, 3, 4, 5, 6, 7, 7]));
