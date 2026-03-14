5a05fb0b1d8c95a5c3040837e9879bd27b777ce2
chandrahaswtw <chandrahaswtw@gmail.com>
2026-03-14 20:28:28 +0530

    array product sum

--- core-algorithms/1_recursion/5_array_product_sum.ts
+++ core-algorithms/1_recursion/5_array_product_sum.ts
@@ -11,7 +11,7 @@ First level depth 1, second level array has 2 and so on.
 
 Our duty is to find the sum multiplied by depth as below:
 
-1 + 2 + (2 + 3)*2 + 6 + ((2 + 3)*3 + 1)*2 + 7
+1 + 2 + (2 + 3)*2 + 6 + ((2 + 3)*3 + 1)*2 + 7 = 58
 
 */
 
