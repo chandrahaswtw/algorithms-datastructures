2c00d41849e36e61987e6f5b722ae4704be12f16
chandrahaswtw <chandrahaswtw@gmail.com>
2026-03-14 20:26:18 +0530

    array product sum

---
+++ core-algorithms/1_recursion/5_product_sum.ts
@@ -0,0 +1,30 @@
+/*
+-----------------
+ARRAY PRODUCT SUM
+-----------------
+
+Say the array is as below:
+
+[1, 2, [2, 3], 6, [[2, 3], 1], 7]
+
+First level depth 1, second level array has 2 and so on.
+
+Our duty is to find the sum multiplied by depth as below:
+
+1 + 2 + (2 + 3)*2 + 6 + ((2 + 3)*3 + 1)*2 + 7
+
+*/
+
+const product_sum = (arr: any, depth = 1) => {
+  let sum = 0;
+  for (let i of arr) {
+    if (Array.isArray(i)) {
+      sum = sum + product_sum(i, depth + 1);
+    } else {
+      sum = sum + i;
+    }
+  }
+  return sum * depth;
+};
+
+console.log(product_sum([1, 2, [2, 3], 6, [[2, 3], 1], 7]));
