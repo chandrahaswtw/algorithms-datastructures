5f40efaeb699114efaf8c31ad018875f6275d581
chandrahaswtw <chandrahaswtw@gmail.com>
2026-04-16 16:33:41 +0530

    hashmapbasic

--- data-structures/4_hash_map/1_hashmapbasic/hashmapbasic.py
+++ data-structures/4_hash_map/1_hashmapbasic/hashmapbasic.py
@@ -1,6 +1,7 @@
 class HashMapBasic:
     def __init__(self):
-        self.bucket = [None] * 100
+        self.bucketSize = 100
+        self.bucket = [None] * self.bucketSize
 
     # Generate hash
     def getHash(self,key):
@@ -17,9 +18,19 @@ class HashMapBasic:
         hash = self.getHash(key)
         return self.bucket[hash]
     
+    def delete(self, key):
+        # We aren't using del or pop() as they remove the element and bucket size decreases
+        hash = self.getHash(key)
+        self.bucket[hash] = None 
+    
+    def contains(self, key):
+        hash = self.getHash(key)
+        return self.bucket[hash] is not None
 
 hashMap = HashMapBasic()
 hashMap["name"] = "chandrahas"
 hashMap["age"] = 34
-
 print(hashMap["name"], hashMap["age"])
+hashMap.delete("name")
+print(hashMap["name"])
+print(hashMap.contains("name"), hashMap.contains("age"))
--- data-structures/4_hash_map/2_chaining/chaining.ts
+++ data-structures/4_hash_map/2_chaining/chaining.ts
@@ -1,26 +1,3 @@
-/*
-- We have a bucket an array of 100.
-- We calculate the hash --> We sum up all ASCII of individual key and we return the modulus by dividing with size. 
-  - This generates a value between 0 and 100.
-  - If value exists at the index (Hash collision), it will add them in the array. So the array looks like below after hash collisions.
-  [
-    [],
-    [
-      { key: "age", value: 34 },
-      { key: "sex", value: "Male" },
-      { key: "Country", value: "India" },
-      { key: "IsMarried", value: true }
-    ],
-    [
-      { key: "name", value: "Chandrahas" },
-      { key: "place", value: "Hyderabad" }
-    ],
-    [],
-    []
-  ]
-
-*/
-
 interface IHash {
   key: string;
   value: any;
