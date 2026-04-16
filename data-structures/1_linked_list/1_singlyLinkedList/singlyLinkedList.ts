a8ab5960ae15796fe446892a83cf645a5c8ee511
chandrahaswtw <chandrahaswtw@gmail.com>
2026-04-16 12:00:44 +0530

    linked list

--- data-structures/1_linked_list/1_singlyLinkedList/singlyLinkedList.ts
+++ data-structures/1_linked_list/1_singlyLinkedList/singlyLinkedList.ts
@@ -1,149 +1,87 @@
-interface ISingleNode<T> {
+interface INode<T> {
   value: T;
-  next: null | ISingleNode<T>;
+  next: INode<T> | null;
 }
 
-class SinglyLinkedList<T> {
-  head: ISingleNode<T> | null = null;
-  tail: ISingleNode<T> | null = null;
+class SingleLinkedList<T> {
+  head: INode<T> | null = null;
 
-  /**
-   *
-   * @param value
-   * @returns node
-   *
-   * This generates a new node based on incoming value.
-   */
-  generateNode(value: T): ISingleNode<T> {
+  generateNode(value: T): INode<T> {
     return {
-      value: value,
+      value,
       next: null,
     };
   }
 
-  /**
-   *
-   * @param value
-   *
-   * This appends a new node towards the end based on incoming value.
-   */
+  traverse() {
+    let currentNode = this.head;
+    while (currentNode) {
+      console.log(currentNode.value);
+      currentNode = currentNode.next;
+    }
+  }
+
   append(value: T) {
     const newNode = this.generateNode(value);
-
-    // Happens for the first insertion
     if (!this.head) {
       this.head = newNode;
-      this.tail = newNode;
-    }
-
-    // When tail exists, it appends towards the last
-    this.tail!.next = newNode;
-    this.tail = newNode;
-  }
-
-  /**
-   * Traverses through the linked list
-   */
-  traverse() {
-    let current = this.head;
-
-    while (current) {
-      console.log(current.value);
-      current = current.next;
-    }
-  }
-
-  /**
-   *
-   * @param value
-   *
-   * This appends node towards the start of list.
-   */
-  prepend(value: T) {
-    const newNode = this.generateNode(value);
-
-    // Happens for the first insertion
-    if (!this.head) {
-      this.append(value);
       return;
     }
 
-    newNode.next = this.head;
-    this.head = newNode;
+    let currentNode = this.head;
+    while (currentNode.next) {
+      currentNode = currentNode.next;
+    }
+
+    //Append to the last node
+    currentNode.next = newNode;
   }
 
-  /**
-   *
-   * @param targetVal - This is the value node after which the below node gets created.
-   * @param newValue - THis is the new node that gets inserted after the findVal.
-   */
-  findAndInsertAfter(targetVal: T, newValue: T) {
-    let targetNode: ISingleNode<T> | null;
-
+  insertAfter(targetVal: T, newValue: T) {
     let currentNode = this.head;
-
     while (currentNode) {
       if (currentNode.value === targetVal) {
         const newNode = this.generateNode(newValue);
         newNode.next = currentNode.next;
         currentNode.next = newNode;
-        break;
+        return;
       }
       currentNode = currentNode.next;
     }
-
-    if (!currentNode) {
-      console.log("Target element is not found");
-    }
   }
 
-  /**
-   *
-   * @param value - Deletes the value
-   *
-   */
   delete(value: T) {
-    //During first iteration, the previous node is null
-    let previousNode: ISingleNode<T> | null = null;
-
+    let prevNode: INode<T> | null = null;
     let currentNode = this.head;
 
     while (currentNode) {
       if (currentNode.value === value) {
-        // Case when the current node is the head and no previous node
-        if (!previousNode) {
+        // If the node is head
+        if (!prevNode) {
           this.head = currentNode.next;
           return;
         }
-
-        previousNode.next = currentNode.next;
+        prevNode.next = currentNode.next;
         return;
       }
-
-      previousNode = currentNode;
+      prevNode = currentNode;
       currentNode = currentNode.next;
-    }
-
-    while (!currentNode) {
-      console.log("Target element is not found");
     }
   }
 }
 
-const sl = new SinglyLinkedList<number>();
+const linkedList = new SingleLinkedList();
+linkedList.append(5);
+linkedList.append(6);
+linkedList.append(7);
+linkedList.append(8);
+// linkedList.traverse();
 
-// Append new nodes
-sl.append(23);
-sl.append(45);
+// linkedList.insertAfter(6, 88);
+// linkedList.traverse();
 
-// Prepend
-sl.prepend(3);
-
-// Find 23 and insert after 99
-sl.findAndInsertAfter(23, 99);
-
-//Delete
-sl.delete(3);
-
-//Traverse
-sl.traverse();
+//Removing head
+linkedList.delete(5);
+//Remove some node in middle
+linkedList.delete(7);
+linkedList.traverse();
